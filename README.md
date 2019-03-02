9fd5a8ff9369ba3fd8e12616cc8200138b0181dc
Unknown <hewitt.archie@yandex.ru>
2019-03-02 20:26:49 +0300

    Added lgtm

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 [![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](/LICENSE)
 [![codecov](https://codecov.io/gh/GillianGrayson/oqs/branch/master/graph/badge.svg)](https://codecov.io/gh/GillianGrayson/oqs)
 
+[![Total alerts](https://img.shields.io/lgtm/alerts/g/GillianGrayson/oqs.svg?logo=lgtm&logoWidth=18)](https://lgtm.com/projects/g/GillianGrayson/oqs/alerts/)
+[![Language grade: C/C++](https://img.shields.io/lgtm/grade/cpp/g/GillianGrayson/oqs.svg?logo=lgtm&logoWidth=18)](https://lgtm.com/projects/g/GillianGrayson/oqs/context:cpp)
+
 | Build status          | Systems / Compilers         |
 | ------------- | ------------------------------------------ |
 | [![CLang / GCC / XCode Builds](https://travis-ci.org/GillianGrayson/oqs.svg?branch=master)](https://travis-ci.org/GillianGrayson/oqs) | Linux (clang / gcc) OSX (XCode clang) |
--- lib/include/calc.h
+++ lib/include/calc.h
@@ -4,7 +4,7 @@
 #ifndef CALC_HPP
 #define CALC_HPP
 
-// MS compatible compilers support #pragma once
+ // MS compatible compilers support #pragma once
 #if defined(_MSC_VER)
 #pragma once
 #endif
@@ -17,63 +17,63 @@
 
 namespace ModernCppCI {
 
-template <class Type>
-using Dictionary = std::map<std::string, Type>;
+	template <class Type>
+	using Dictionary = std::map<std::string, Type>;
 
-namespace DefaultOperations {
-const Operation Plus = [](const int &value1, const int &value2) {
-  return value1 + value2;
-};
-const Operation Minus = [](const int &value1, const int &value2) {
-  return value1 - value2;
-};
-const Operation Times = [](const int &value1, const int &value2) {
-  return value1 * value2;
-};
-const Operation Div = [](const int &value1, const int &value2) {
-  return value1 / value2;
-};
-const Operation Zero = [](const int &value1, const int &value2) { return 0; };
-}  // namespace DefaultOperations
+	namespace DefaultOperations {
+		const Operation Plus = [](const int &value1, const int &value2) {
+			return value1 + value2;
+		};
+		const Operation Minus = [](const int &value1, const int &value2) {
+			return value1 - value2;
+		};
+		const Operation Times = [](const int &value1, const int &value2) {
+			return value1 * value2;
+		};
+		const Operation Div = [](const int &value1, const int &value2) {
+			return value1 / value2;
+		};
+		const Operation Zero = [](const int &value1, const int &value2) { return 0; };
+	}  // namespace DefaultOperations
 
-class Calc {
- public:
-  Calc() noexcept {};
+	class Calc {
+	public:
+		Calc() noexcept {};
 
-  Calc(const Calc &other) noexcept
-      : operations_{other.operations_}, steps_{other.steps_} {}
+		Calc(const Calc &other) noexcept
+			: operations_{ other.operations_ }, steps_{ other.steps_ } {}
 
-  Calc &operator=(const Calc &other) noexcept;
+		Calc &operator=(const Calc &other) noexcept;
 
-  auto total_operations() const noexcept { return operations_.size(); }
+		auto total_operations() const noexcept { return operations_.size(); }
 
-  void add_step(const CalcStep &step);
+		void add_step(const CalcStep &step);
 
-  void add_step(const int value);
+		void add_step(const int value);
 
-  void add_step(const std::string &operation_name);
+		void add_step(const std::string &operation_name);
 
-  auto total_steps() const noexcept { return steps_.size(); }
+		auto total_steps() const noexcept { return steps_.size(); }
 
-  int result() const;
+		int result() const;
 
-  template <typename T>
-  Calc operator<<(const T &value) const {
-    Calc new_calc{*this};
-    new_calc.add_step(value);
-    return new_calc;
-  }
+		template <typename T>
+		Calc operator<<(const T &value) const {
+			Calc new_calc{ *this };
+			new_calc.add_step(value);
+			return new_calc;
+		}
 
-  friend std::ostream &operator<<(std::ostream &stream, const Calc &calc);
+		friend std::ostream &operator<<(std::ostream &stream, const Calc &calc);
 
- private:
-  std::list<CalcStep> steps_{};
-  Dictionary<Operation> operations_{{"+", DefaultOperations::Plus},
-                                    {"-", DefaultOperations::Minus},
-                                    {"*", DefaultOperations::Times},
-                                    {"/", DefaultOperations::Div}};
-  static Logger logger_;
-};
+	private:
+		std::list<CalcStep> steps_{};
+		Dictionary<Operation> operations_{ {"+", DefaultOperations::Plus},
+										  {"-", DefaultOperations::Minus},
+										  {"*", DefaultOperations::Times},
+										  {"/", DefaultOperations::Div} };
+		static Logger logger_;
+	};
 
 }  // namespace ModernCppCI
 
